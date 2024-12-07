5335d74b84952a04e1db6b0039ca83810da32849
VK132211 <m.vk.80899@gmail.com>
2024-12-07 09:39:18 +0530

    update PlayGame

--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import MaskedText from "../../components/MaskedText/MaskedText";
 import LetterButtons from "../../components/LetterButtons/LetterButtons";
@@ -6,9 +6,25 @@ import HangMan from "../../components/HangMan/HangMan";
 function PlayGame() {
   const [usedLetters, setUsedLetters] = useState([]);
   const [step, setStep] = useState(0);
+  const [gameStatus, setGameStatus] = useState(null);
+  const maxSteps = 7;
 
   const location = useLocation();
   const wordSelected = location.state?.wordSelected;
+
+  useEffect(() => {
+    if (step > maxSteps) {
+      setGameStatus("lose");
+    } else if (
+      wordSelected &&
+      wordSelected
+        .toUpperCase()
+        .split("")
+        .every((letter) => usedLetters.includes(letter))
+    ) {
+      setGameStatus("win");
+    }
+  }, [step, usedLetters, wordSelected]);
 
   const handleLetterClick = function (letter) {
     if (wordSelected.toUpperCase().includes(letter)) {
@@ -23,20 +39,29 @@ function PlayGame() {
   return (
     <div>
       <h1>Play Game</h1>
-      <MaskedText text={wordSelected} usedLetters={usedLetters} />
-      <hr />
-      <div className="flex justify-between items-center">
-        <div className="basis-2/4">
-          <LetterButtons text={wordSelected} usedLetters={usedLetters} onLetterClick={handleLetterClick} />
-        </div>
-        <div className="basis-2/4 flex justify-center items-center">
-          <HangMan step={step} />
-        </div>
-      </div>
+
+      {gameStatus === "win" ? (
+        <div className="text-green-500 text-2xl">🎉 Congratulations! You guessed the word! 🎉</div>
+      ) : gameStatus === "lose" ? (
+        <div className="text-red-500 text-2xl">💀 Game Over! The word was "{wordSelected}". 💀</div>
+      ) : (
+        <>
+          <MaskedText text={wordSelected} usedLetters={usedLetters} />
+          <hr />
+          <div className="flex justify-between items-center">
+            <div className="basis-2/4">
+              <LetterButtons text={wordSelected} usedLetters={usedLetters} onLetterClick={handleLetterClick} />
+            </div>
+            <div className="basis-2/4 flex justify-center items-center">
+              <HangMan step={step} />
+            </div>
+          </div>
+        </>
+      )}
+
       <hr />
       <Link to="/start" className="text-blue-600 underline hover:text-blue-700">
-        {" "}
-        Start Game{" "}
+        Start Game
       </Link>
     </div>
   );
